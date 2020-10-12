81a3b2ab7fe0bb3d8354f3b9865a58e179906502
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2020-10-12 10:22:59 +0600

    refactor android support sdk

--- android/app/src/main/java/com/sagar/gossip/SecondActivity.kt
+++ android/app/src/main/java/com/sagar/gossip/SecondActivity.kt
@@ -2,7 +2,6 @@ package com.sagar.gossip
 
 import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
